f33f85fc7a345ad21e4dc83a88afec7a6fbfcb38
Sansevieria <elizabethleighmather@gmail.com>
2019-04-08 00:48:28 -0500

    edit

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
 $(document).ready(function() {
     console.log("Hello");
-    console.log("hellooooo");
-    console.log("heoo");
-    console.log("hi");
-    console.log("Hello");
-    console.log("Hello");
-    console.log("Hello");
 });
