dfd2658681aff3d0b8ca3c0dccd7d428fcdfea91
Sansevieria <elizabethleighmather@gmail.com>
2019-04-07 23:55:22 -0500

    push to github

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 $(document).ready(function() {
     alert("Hello");
-<<<<<<< HEAD
     alert("hellooooo");
     alert("heoo");
     alert("hi");
-=======
     alert("Hello");
     alert("Hello");
     alert("Hello");
->>>>>>> ce85a2d39ddd974b5f53697a0f3abfd1541e165e
 });
